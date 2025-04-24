902db18708a20bffb5b9fc8217ae7d76e125d8d6
Isha Bhandari <isha.bhandari101@gmail.com>
2025-04-24 11:29:50 -0400

    First attempt at mutate function

--- Genetic_Algo/mutation.py
+++ Genetic_Algo/mutation.py
@@ -1,5 +1,16 @@
 from Poker.player import Player
+import uuid
+import random
 
 # Implement mutation strategy and return a player object
-def mutate() -> Player:
-    pass
+# Mutation will take a player's traits and adjust them randomly by a random amount (0-0.9 in a circular way)
+def mutate(player: Player) -> Player:
+    mutated_player = Player(str(uuid.uuid4())[:8])
+    mutated_player.traits = player.traits.copy()
+
+    for trait in mutated_player.traits:
+        amnt_change_trait_by = random.random()
+        # Circular wrap 
+        mutated_player.traits[trait] = (mutated_player.traits[trait] + amnt_change_trait_by) % 1.0
+
+    return mutated_player
