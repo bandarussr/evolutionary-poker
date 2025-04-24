8c44ebd4a3b837e20ffbd9e6d46f4b8c9a13d4a0
Isha Bhandari <isha.bhandari101@gmail.com>
2025-04-24 11:36:32 -0400

    First attempt at tournament selection function

--- Genetic_Algo/selection.py
+++ Genetic_Algo/selection.py
@@ -1,4 +1,7 @@
 from Poker.player import Player
-#TODO:: should select a random group of k players within population List[Player] and within that return the player with the best fitness
+import random
+#Select a random group of k players within population List[Player] and within that return the player with the best fitness
 def tournament_selection(k, population) -> Player:
-    pass
+    randomly_chosen_players = random.sample(population, k)
+    # Chooses and returns player with highest fitness score
+    return max(randomly_chosen_players, key=lambda player: player.fitness)
